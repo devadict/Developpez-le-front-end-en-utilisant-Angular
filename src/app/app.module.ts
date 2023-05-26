c4330c440d0405cb365aa35faae144bb55dc2d4b
hakimsy <hakimsy17@gmail.com>
2023-05-26 13:03:36 +0000

    Updated the app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
+import {ChartModule} from "primeng/chart";
+import {CardModule} from 'primeng/card';
+
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, CountryDetailComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ChartModule, CardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
