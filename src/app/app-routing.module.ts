9dfc6dc122b0cca3838de1d600b683f8fc063664
hakimsy <hakimsy17@gmail.com>
2023-05-26 13:13:11 +0000

    Updated routes of the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
+import {NgForOf, NgIf} from "@angular/common";
+import {CountryDetailComponent} from "./pages/country-detail/country-detail.component";
 
 const routes: Routes = [
   {
@@ -9,13 +11,18 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: '**', // wildcard
+    path: ':id',
+    component: CountryDetailComponent
+  },
+  {
+    path: 'not-found',
     component: NotFoundComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes), NgForOf, NgIf],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
